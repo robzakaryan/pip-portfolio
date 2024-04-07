b8662a7f4b652ef421e1914b305584af5b1781ea
robzakaryan <robzakaryan@gmail.com>
2024-04-07 15:33:27 +0400

    add password to env var

--- send_email.py
+++ send_email.py
@@ -1,12 +1,13 @@
 import smtplib
 import ssl
+import os
 
 
 def email(message: str):
     host = 'smtp.gmail.com'
     port = 465
     username = 'robzakaryan@gmail.com'
-    password = 'uwap higg vjmw ogkp'
+    password = os.getenv('PASSWORD_GMAIL_SMTP')
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
         server.login(username, password)
